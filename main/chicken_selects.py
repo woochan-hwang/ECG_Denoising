f25fd94910558e1d2a8c179d6800460ba5b9517f
Woochan Hwang <wh1714@ic.ac.uk>
2018-03-27 21:43:34 +0100

    first commit

---
+++ main/chicken_selects.py
@@ -0,0 +1,287 @@
+'''
+Data retreival and preprocessing for specific training environments
+
+Written by Woochan H. (Last Revised 28/03/18)
+
+This module allows you to conveniently open and process data objects for specific environment settings.
+It will automatically set CUDA, file directory settings accordingly with minimal effort.
+
+The EnvSetter class sets the details of the training environment.
+The WFDBData and EMGData class opens each dataset. They contain methods that allow you to open
+all the data contained in the folder and concat it to one variable, or each file individually if you wish.
+The WFDBData also has methods that allow you to add gaussian noise / baseline_wander.
+Processor contains methods of reorganizing data in a way appropriate for neural networks.
+
+The Data class inherits all the methods of the other classes, you should create a class
+instance for the Data class and use the methods from the parent classes.
+
+I tried my best to annotate each method and make them flexible. However, if you keep to the same file structure
+as the /main file I uploaded, you should be able to use the default settings without having to change much.
+Ideally, this file should be kept within the /main file as well.
+
+Please refer to sample_sae.py and sample_drdae.py for example use.
+'''
+
+import os
+import wfdb
+import torch
+import numpy as np
+from numpy import genfromtxt
+
+# Describing training environment
+class EnvSetter(object):
+
+    # Default cuda is False. Default filepath is current dir
+    def __init__(self, cuda = False):
+        self.cuda = cuda
+        self.filepath = os.getcwd()
+
+    # Set filepath to path of specific file
+    def set_filepath(self, name):
+        for root, dirs, files in os.walk(os.getcwd()):
+            if name in files:
+                self.filepath = os.path.join(root, name)
+
+    # Only use this if you wish to change the host path for some reason. Default is set to my setting.
+    def change_filepath(self, new_path):
+        self.filepath = new_path
+        print("Parent filepath changed. Consider Resetting ECG,EMG,ACC paths")
+
+    # Use to set back to current dir
+    def default_filepath(self):
+        self.filepath = os.getcwd()
+        print("Parent filepath changed. Consider Resetting ECG,EMG,ACC paths")
+
+    def get_filepath(self):
+        return self.filepath
+
+    # Set CUDA setting. Default is False
+    def cuda_on(self):
+        self.cuda = True
+        print("CUDA enabled")
+
+    def cuda_off(self):
+        self.cuda = False
+        print("CUDA disabled")
+
+
+# Object for getting data from MIT-BIH Arrythmia Database
+class WFDBData(EnvSetter):
+
+    def __init__(self):
+        super(WFDBData, self).__init__()
+        self.ecgpath = self.filepath
+
+    def set_ecg_filepath(self, filepath = 'ecgdata'):
+        self.ecgpath = str(self.get_filepath()) + '/' + str(filepath)
+        print('ECGs will be imported from {}'.format(self.ecgpath))
+
+    def get_ecg_filepath(self):
+        return self.ecgpath
+
+    # Pull all data using wfdb format. Use pull_signal to get raw data.
+    def pull_wfdb(self, filename, t0 = 0, tf = int(30 * 360 * 60)):
+        return wfdb.rdsamp("{}/{}".format(self.get_ecg_filepath(), filename), sampfrom = t0, sampto = tf, channels = [0])
+
+    # Search and extract all data in the file_path using wfdb format. Use pull_all_signal to extract signal.
+    def pull_all_wfdb(self, t0 = 0, tf = int(30 * 360 * 60)):
+        items = os.listdir(self.get_ecg_filepath())
+        items.sort()
+        newlist = []
+        namelist = []
+        for name in items:
+            if name.endswith(".dat"):
+                namelist.append(name)
+                name = name[:-4]
+                data = wfdb.rdsamp("{}/{}".format(self.get_ecg_filepath(), name), sampfrom = t0, sampto = tf, channels = [0])
+                newlist.append(data)
+        print('These are the files[ECG] opened from the dir: {}'.format(namelist))
+        return newlist
+
+    # Save raw ecg signal from the wfdb format. 30 min each. 360Hz. 11bit. 10mV.
+    def pull_ecg(self, filename, t0 = 0, tf = int(20 * 360 * 60)):
+        temp_data = self.pull_wfdb(filename, t0 = 0, tf = int(30 * 360 * 60))
+        temp_data = temp_data.p_signals
+        out_data = np.reshape(temp_data,(1, len(temp_data)))
+        return out_data
+
+    # Output raw ecg signal from all .dat files in file_path.
+    def pull_all_ecg(self, t0 = 0, tf = int(30 * 360 * 60)):
+        output = []
+        for data in self.pull_all_wfdb(t0, tf):
+            signal = list(data.p_signals)
+            output.append(signal)
+        output = np.array(output)
+        output = np.reshape(output, (1, np.shape(output)[0]*np.shape(output)[1]))
+#        print('Created signal(numpy array) with shape: {}'.format(np.shape(output)))
+        return output
+
+    # Set parameters for guassian noise
+    def set_gaussian(self, signal, amp = 1, u = 0, std = 0.01):
+        self.gaussian = np.multiply(amp, np.random.normal(u, std, len(signal)))
+
+    def get_gaussian(self):
+        return self.gaussian
+
+    # Set parameters for baseline wander
+    def set_baseline_wander(self, signal, respf = 12, amp = 0.05):
+        N = float(len(signal))
+        resppm = (float(respf)/60)
+        ix = np.arange(N)
+        self.baseline_wander = amp*np.sin(2*np.pi/(resppm*(N/2))*ix)
+
+    def get_baseline_wander(self):
+        return self.baseline_wander
+
+    # Noisy signal with Gaussian and BW. Must be called after setting both
+    def noisy_signal(self, raw_signal):
+        return raw_signal + self.get_gaussian() + self.get_baseline_wander()
+
+
+# Object for opening EMG and ACC data_csv format created from Joshua's Matlab script
+class EMGData(EnvSetter):
+
+    def __init__(self):
+        super(EMGData, self).__init__()
+        self.emgpath = self.filepath
+        self.accpath = self.filepath
+
+    # Specify filepath for EMG within motherpath
+    def set_emg_filepath(self, filepath = 'emgdata'):
+        self.emgpath = str(self.get_filepath()) + '/' + str(filepath)
+        print('EMGs will be imported from {}'.format(self.emgpath))
+
+    def get_emg_filepath(self):
+        return self.emgpath
+
+    # Specify filepath for ACC withint motherpath
+    def set_acc_filepath(self, filepath = 'accdata'):
+        self.accpath = str(self.get_filepath()) + '/' + str(filepath)
+        print('ACCs will be imported from {}'.format(self.accpath))
+
+    def get_acc_filepath(self):
+        return self.accpath
+
+    # Pull individual signals
+    def pull_emg(self, filename):
+        return genfromtxt("{}/{}.csv".format(self.get_emg_filepath(), filename), delimiter = ',')
+
+    def pull_acc(self, filename):
+        acc = genfromtxt("{}/{}.csv".format(self.get_acc_filepath(), filename), delimiter = ',')
+        return acc[:,1:4].transpose()
+
+    # Pull all relevant(.csv) signals in folder
+    def pull_all_emg(self):
+        file_path = self.get_emg_filepath()
+        items = os.listdir(file_path)
+        items.sort()
+        newlist = []
+        namelist = []
+        for name in items:
+            if name.endswith(".csv"):
+                namelist.append(name)
+                name = name[:-4]
+                data = self.pull_emg(filename = name)
+                newlist.append(data)
+        print('These are the files[EMG] opened from the dir: {}'.format(namelist))
+        signal = np.reshape(np.array(newlist), (1, -1))
+        return signal
+
+    def pull_all_acc(self):
+        file_path = self.get_acc_filepath()
+        items = os.listdir(file_path)
+        items.sort()
+        newlist = []
+        namelist = []
+        for name in items:
+            if name.endswith(".csv"):
+                namelist.append(name)
+                name = name[:-4]
+                data = self.pull_acc(filename = name)
+                newlist.append(data)
+        print('These are the files[ACC] opened from the dir: {}'.format(namelist))
+        signal = np.reshape(np.array(newlist), (3, -1))
+        return signal
+
+
+# Common data processing for neural network training
+class Processor(EnvSetter):
+
+    def __init__(self):
+        super(Processor, self).__init__()
+
+    # Change data format to specified form / length
+    # Format 1: <ECG..., X..., Y..., Z...>; each * feature_len
+    # Format 2: in process
+    # Total lenght being 4 * feature_len
+    def reformat(self, data, data_form = 0, feature_len = 0):
+        self.format = data_form
+        self.feature_len = feature_len
+
+        if data_form == 0 or feature_len == 0:
+            print("Please specify format type and length")
+        else:
+            if data_form == 1:
+                output = self.format1(data, feature_len)
+            elif data_form == 2:
+                print('Format 2 unavailable at the moment')
+#                output = _format2(data, feature_len)
+            else:
+                print("Undefined format type")
+        return output
+
+    # Revert output to numpy of original format. Accepts numpy not tensor object
+    # To be used to plot denoised results
+    def undo_reformat(self, data):
+        if self.format == 1:
+            output = self.undo_format1(data, self.feature_len)
+        elif self.format == 2:
+            print('Format 2 unavailable at the moment')
+#            output = _undo_format2(data, self.feature_len)
+        else:
+            print('Unalbe to undo format')
+        return output
+
+    # These functions should not be called directly
+    def format1(self, input_arr, feature_len):
+        l = int(np.shape(input_arr)[1])
+        k = int(feature_len)
+        sample_num = int(l / feature_len)
+        output = np.zeros((sample_num, 4*k))
+        for i in range(0,sample_num):
+            output[i, 0:k] = input_arr[0, k*i:k*(i+1)]
+            output[i, k:2*k] = input_arr[1, k*i:k*(i+1)]
+            output[i, 2*k:3*k] = input_arr[2, k*i:k*(i+1)]
+            output[i, 3*k:4*k] = input_arr[3, k*i:k*(i+1)]
+        return np.array(output)
+
+    def undo_format1(self, npver, feature_len):
+        k = int(feature_len)
+        sample_num = np.shape(npver)[0]
+        sig_len = sample_num*k
+        output = np.zeros((4,sig_len))
+        for i in range(0,sample_num):
+            output[0, k*i:k*(i+1)] = npver[i, 0:k]
+            output[1, k*i:k*(i+1)] = npver[i, k:2*k]
+            output[2, k*i:k*(i+1)] = npver[i, 2*k:3*k]
+            output[3, k*i:k*(i+1)] = npver[i, 3*k:4*k]
+        return np.array(output)
+
+    # Formating into tensors usable in PyTorch. This is currently NOT wrapped in a Variable
+    def to_tensor(self, data):
+        return torch.from_numpy(data).float()
+
+    def to_numpy(self, tensor):
+        return tensor.data.numpy()
+
+
+# Used to create Input/Label data for RNN models
+class Data(WFDBData, EMGData, Processor):
+
+    def __init__(self, model, motion, noiselevel, cuda = False):
+        self.model = model
+        self.motion = motion
+        self.noiselevel = noiselevel
+        WFDBData.__init__(self)
+        EMGData.__init__(self)
+        EnvSetter.__init__(self, cuda)
---
+++ main/sample_sae.py
@@ -0,0 +1,161 @@
+# Sample Stacked denoising autoencoder (2 layers)
+# Note use of chicken_selects module and how to process data imports
+# Written by Woochan H. (Last modified: 26/03/18)
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import numpy as np
+import matplotlib.pyplot as plt
+from chicken_selects import *
+
+# Object Data('model type', 'motion', noiselevel, cuda = False)
+data = Data('Stacked Autoencoder', 'flexion_extension', 1)
+
+if torch.cuda.is_available == True:
+    data.cuda_on()
+
+# Specify directory if you have changed folder name / dir
+data.set_ecg_filepath()
+data.set_emg_filepath()
+data.set_acc_filepath()
+
+# Call data into numpy array format. Check soure code for additional input specifications
+clean_ecg = data.pull_all_ecg(t0 = 0, tf = 30000)
+noisy_ecg = data.pull_all_emg()
+acc_dat = data.pull_all_acc()
+
+# Acc data modified to fit that of noisy emg
+acc_dat = np.array(list(acc_dat[:, 0:3000].transpose())*140).transpose()
+# Clean generated from gaussian dist, N(0, 0.05)
+clean_acc = np.random.randn(np.shape(acc_dat)[0], np.shape(acc_dat)[1]) *0.05
+
+# Add ACC data onto clean/noisy ecg data
+input_dat = np.vstack((noisy_ecg, acc_dat))
+label_dat = np.vstack((clean_ecg, clean_acc))
+
+# Reformat to shape that can be imported to neural net
+input_dat = data.reformat(input_dat, feature_len = 200, data_form = 1)
+label_dat = data.reformat(label_dat, feature_len = 200, data_form = 1)
+print("Input Data shape: {}".format(np.shape(input_dat)))
+print("Label Data shape: {}".format(np.shape(label_dat)))
+print("Step 0: Data Import Done")
+
+#input_dat = data.undo_reformat(input_dat)
+#label_dat = data.undo_reformat(label_dat)
+
+_ = input("Continue(y/n)?: ")
+
+# Change numpy array to tensor variable
+Tensor_Input = Variable(torch.from_numpy(input_dat).float())
+Tensor_Label = Variable(torch.from_numpy(label_dat).float())
+
+# Hyper Parameters
+EPOCH = int(input("Epochs?: "))
+LR = float(input("Learning rate?: "))
+BATCH_SIZE = int(input("Batch size?: "))
+
+# Autoencoder Model Structure
+class StackedAutoEncoder(nn.Module):
+    def __init__(self):
+        super(StackedAutoEncoder, self).__init__()
+
+        self.encoder1 = nn.Sequential(
+            nn.Linear(800, 270),
+            nn.Tanh(),
+            nn.Linear(270, 135),
+            nn.Tanh(),
+            nn.Linear(135, 70),
+        )
+        self.decoder1 = nn.Sequential(
+            nn.Linear(70, 135),
+            nn.Tanh(),
+            nn.Linear(135, 270)
+        )
+        self.encoder2 = nn.Sequential(
+            nn.Linear(270, 135),
+            nn.Tanh(),
+            nn.Linear(135, 70),
+        )
+        self.decoder2 = nn.Sequential(
+            nn.Linear(70, 135),
+            nn.Tanh(),
+            nn.Linear(135, 270),
+            nn.Tanh(),
+            nn.Linear(270, 800)
+        )
+
+    def forward(self, x):
+        en1 = self.encoder1(x)
+        de1 = self.decoder1(en1)
+        en2 = self.encoder2(de1)
+        de2 = self.decoder2(en2)
+        return en1, de1, en2, de2
+
+print("Step 1: Model Setup Done")
+
+# Setting of Loss function and optimizer
+SAE = StackedAutoEncoder()
+optimizer = torch.optim.Adam(SAE.parameters(), lr=LR)
+loss_func = nn.MSELoss()
+train_loss = []
+
+if torch.cuda.is_available() == True:
+    SAE = SAE.cuda()
+
+def save_params(save_name):
+    dir = '{}/Trained_Params/{}/{}'.format(data.filepath, data.model, save_name)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    torch.save(SAE.encoder1, dir + '/encoder1_{}.pt'.format(EPOCH))
+    torch.save(SAE.decoder1, dir + '/decoder1_{}.pt'.format(EPOCH))
+    torch.save(SAE.encoder2, dir + '/encoder2_{}.pt'.format(EPOCH))
+    torch.save(SAE.decoder2, dir + '/decoder2_{}.pt'.format(EPOCH))
+    np.save(dir + '/trainloss_{}.npy'.format(EPOCH),train_loss)
+    print("Step 3: Model Saved")
+
+
+# Train the model
+try:
+    print("Step 2: Model Training Start")
+
+    for epoch in range(EPOCH):
+
+        en1, de1, en2, de2 = SAE(Tensor_Input)
+
+        loss = loss_func(de2, Tensor_Label)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        print('Epoch: ', epoch + 1, '| train loss: %.4f' % loss.data[0])
+        train_loss.append(loss.data[0])
+
+    print("Step 2: Model Training Finished")
+
+    # Plot Loss
+    plt.figure(figsize = (10,4));
+    plt.plot(train_loss, color='k', linewidth=0.4, linestyle='-', label = 'MSE loss');
+    plt.legend(loc = 2);
+    plt.title("Training Loss({} | {} | LR:{})".format(data.model, data.motion, LR));
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+
+    plt.show()
+
+    # Save trained Parameters
+    if str(input("Save Parameters?(y/n): ")) == 'y':
+        save_name = str(input("Save parameters as?: ")) + '_Interrupted'
+        save_params(save_name)
+        print("End of Session")
+    else:
+        print("Session Terminated. Parameters not saved")
+
+
+except KeyboardInterrupt:
+
+    if str(input("Save Parameters?(y/n): ")) == 'y':
+        save_name = str(input("Save parameters as?: ")) + '_Interrupted'
+        save_params(save_name)
+    else:
+        print("Session Terminated. Parameters not saved")
