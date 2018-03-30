6dc064ae3ebf21ba68149d6740529e7c92fc1c5c
Woochan Hwang <wh1714@ic.ac.uk>
2018-03-30 01:50:32 +0100

    conv dimension fixed

--- main/cdae.py
+++ main/cdae.py
@@ -45,14 +45,14 @@ input_dat = np.vstack((noisy_ecg, acc_dat))
 label_dat = np.vstack((clean_ecg, clean_acc))
 
 # Note Use of data_form = 2, which gives a 2D output for each training sample
-input_dat = data.reformat(input_dat, feature_len = 200, data_form = 2)
-label_dat = data.reformat(label_dat, feature_len = 200, data_form = 2)
+input_dat = data.reformat(input_dat, feature_len = 300, data_form = 2)
+label_dat = data.reformat(label_dat, feature_len = 300, data_form = 2)
 print("Input Data shape: {}".format(np.shape(input_dat)))
 print("Label Data shape: {}".format(np.shape(label_dat)))
 
-train_set, test_set = data.data_splitter(input_dat, label_dat, shuffle = True, ratio = 4)
+train_set, val_set = data.data_splitter(input_dat, label_dat, shuffle = True, ratio = 4)
 print(np.shape(train_set))
-print(np.shape(test_set))
+print(np.shape(val_set))
 
 print("CUDA: {}".format(data.cuda))
 print("Step 0: Data Import Done")
@@ -65,9 +65,9 @@ EPOCH = int(input("Epochs?: "))
 LR = float(input("Learning rate?: "))
 BATCH_SIZE = int(input("Batch size?: "))
 
-# Generate tensors for training / testing
+# Generate tensors for training / validation
 train_set = torch.from_numpy(train_set).float()
-test_set = torch.from_numpy(test_set).float()
+val_set = torch.from_numpy(val_set).float()
 
 
 # Autoencoder Model Structure
@@ -75,21 +75,21 @@ class ConvAutoEncoder(nn.Module):
     def __init__(self):
         super(ConvAutoEncoder, self).__init__()
 
+        # Input = b, 1, 4, 300
         self.encoder = nn.Sequential(
-            nn.Conv2d(1, 16, 3, stride=1, padding=2), # B,C,H,W = b, 16, 4, 200
-            nn.ReLU(True),
-            nn.MaxPool2d(2, stride=2), # b, 16, 2, 100
-            nn.Conv2d(16, 8, 3, stride=2, padding=1), # b, 8, 2, 100
-            nn.ReLU(True),
-            nn.MaxPool2d(2, stride=1) # b, 8, 1, 50
+            nn.Conv2d(1, 8, (4,3), stride=1, padding=(0,1)), # b, 8, 1, 300
+            nn.Tanh(),
+            nn.MaxPool2d((1,2), stride=2), # b, 8, 1, 150
+            nn.Conv2d(8, 4, 3, stride=1, padding=1), # b, 8, 1, 150
+            nn.Tanh(),
+            nn.MaxPool2d((1,2), stride=2) # b, 4, 1, 75
         )
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(8, 16, 3, stride=2), # b, 16, 2, 100
-            nn.ReLU(True),
-            nn.ConvTranspose2d(16, 8, 3, stride=2, padding=1), # b, 8, 4, 200
-            nn.ReLU(True),
-            nn.ConvTranspose2d(8, 1, 2, stride=1, padding=1), # b, 1, 4, 200
-            nn.Tanh()
+            nn.ConvTranspose2d(4, 8, 3, stride=2, padding=1, output_padding=(0,1)), # b, 8, 1, 150
+            nn.Tanh(),
+            nn.ConvTranspose2d(8, 8, 3, stride=2, padding=(0,1), output_padding=1), # b, 8, 4, 300
+            nn.Tanh(),
+            nn.ConvTranspose2d(8, 1, 3, stride=1, padding=1), # b, 1, 4, 300
         )
 
     def forward(self, x):
@@ -103,7 +103,7 @@ print("Step 1: Model Setup Done")
 CAE = ConvAutoEncoder()
 optimizer = torch.optim.Adam(CAE.parameters(), lr=LR, weight_decay=1e-5)
 loss_func = nn.MSELoss()
-train_loss, test_loss = [], []
+train_loss, val_loss = [], []
 
 
 def save_model(save_name, optim, loss_f, lr, epoch = EPOCH):
@@ -121,15 +121,15 @@ def save_model(save_name, optim, loss_f, lr, epoch = EPOCH):
                 },
                dir + '/model.pth')
     np.save(dir + '/trainloss.npy',train_loss)
-    np.save(dir + '/testloss.npy',test_loss)
+    np.save(dir + '/valloss.npy',val_loss)
     print("Step 3: Model Saved")
 
 
 # Train the model
 try:
-    # Generates mini_batchs for training. Loads data for testing.
+    # Generates mini_batchs for training. Loads data for validation.
     train_loader = loader.DataLoader(dataset = train_set, batch_size = BATCH_SIZE, shuffle = True)
-    t_x, t_y = Variable(test_set[:,0:1,:,:]), Variable(test_set[:,1:2,:,:])
+    t_x, t_y = Variable(val_set[:,0:1,:,:]), Variable(val_set[:,1:2,:,:])
     print("t_x", t_x.size())
 
     # Moves data and model to gpu if available
@@ -161,12 +161,12 @@ try:
             loss.backward()
             optimizer.step()
 
-        # Evaluates current model state on test data set
+        # Evaluates current model state on val data set
         pred_y = CAE(t_x)
-        loss_test_set = loss_func(pred_y, t_y)
-        print('Epoch: {} | train loss: {:.4f} | test loss: {:.4f}'.format(epoch+1, loss.data[0], loss_test_set.data[0]))
+        loss_val_set = loss_func(pred_y, t_y)
+        print('Epoch: {} | train loss: {:.4f} | val loss: {:.4f}'.format(epoch+1, loss.data[0], loss_val_set.data[0]))
         train_loss.append(loss.data[0])
-        test_loss.append(loss_test_set.data[0])
+        val_loss.append(loss_val_set.data[0])
 
     print("Step 2: Model Training Finished")
 
@@ -187,16 +187,16 @@ else:
         print("Parameters not saved")
 
     # Plot Loss
-    threshold_train = 100*min(train_loss)
-    threshold_test = 100*min(test_loss)
+    threshold_train = 500*min(train_loss)
+    threshold_val = 500*min(val_loss)
     for i in range(len(train_loss)):
         if train_loss[i] > threshold_train:
             train_loss[i] = threshold_train
-        if test_loss[i] > threshold_test:
-            test_loss[i] = threshold_test
+        if val_loss[i] > threshold_val:
+            val_loss[i] = threshold_val
     plt.figure(figsize = (10,4));
     plt.plot(train_loss, color='k', linewidth=0.4, linestyle='-', label = 'train_set loss');
-    plt.plot(test_loss, color='b', linewidth=0.4, linestyle='-', label = 'test_set loss')
+    plt.plot(val_loss, color='b', linewidth=0.4, linestyle='-', label = 'val_set loss')
     plt.legend(loc = 2);
     plt.title("Training Loss({} | {} | LR:{})".format(data.model, data.motion, LR));
     plt.xlabel("Epochs")
