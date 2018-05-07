bcc73a202616189765e56bbec0a8a04b12571c30
Woochan Hwang <wh1714@ic.ac.uk>
2018-05-07 15:00:28 +0100

    v2 change

--- main/cdae_v2.py
+++ main/cdae_v2.py
@@ -104,21 +104,19 @@ class ConvAutoEncoder(nn.Module):
         # Zero padding is almost the same as average padding in this case
         # Input = b, 1, 4, 300
         self.encoder = nn.Sequential(
-            nn.Conv2d(1, 8, (4,3), stride=1, padding=(0,1)), # b, 8, 1, 300
+            nn.Conv2d(1, 16, (4,3), stride=1, padding=(0,1)), # b, 16, 1, 300
             nn.Tanh(),
-            nn.MaxPool2d((1,2), stride=2), # b, 8, 1, 150
-            nn.Conv2d(8, 8, 3, stride=1, padding=1), # b, 8, 1, 150
+            nn.MaxPool2d((1,2), stride=2), # b, 16, 1, 150
+            nn.Conv2d(16, 8, 3, stride=1, padding=1), # b, 8, 1, 150
             nn.Tanh(),
-            nn.MaxPool2d((1,2), stride=2), # b, 8, 1, 75
-            nn.Conv2d(8, 4, 3, stride=1, padding=1), # b, 4, 1, 150
-            nn.Tanh(),
+            nn.MaxPool2d((1,2), stride=2) # b, 8, 1, 75
         )
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(4, 8, 3, stride=2, padding=1, output_padding=(0,1)), # b, 8, 1, 150
+            nn.ConvTranspose2d(8, 8, 3, stride=2, padding=1, output_padding=(0,1)), # b, 8, 1, 150
             nn.Tanh(),
-            nn.ConvTranspose2d(8, 8, 3, stride=2, padding=(0,1), output_padding=1), # b, 8, 4, 300
+            nn.ConvTranspose2d(8, 4, 3, stride=2, padding=(0,1), output_padding=1), # b, 4, 4, 300
             nn.Tanh(),
-            nn.ConvTranspose2d(8, 1, 3, stride=1, padding=1), # b, 1, 4, 300
+            nn.ConvTranspose2d(4, 1, 3, stride=1, padding=1), # b, 1, 4, 300
         )
 
     def forward(self, x):
