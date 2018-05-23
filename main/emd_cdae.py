edd107550f70bf712c3ebc2a72d4f0352ddb549e
Woochan Hwang <wh1714@ic.ac.uk>
2018-05-23 10:33:37 +0100

    emd working ver

--- main/emd_cdae.py
+++ main/emd_cdae.py
@@ -18,12 +18,12 @@ from chicken_selects import *
 import matplotlib.pyplot as plt
 from PyEMD import EMD
 
-print(torch.__version__)
+print("Torch version: ", torch.__version__)
 
 #noiselevel = int(input("EMG noise level?: "))
 noiselevel = 3
 # Object Data('model type', 'motion', noiselevel, cuda = False)
-data = Data('Convolutional Autoencoder', 'mixed', noiselevel = noiselevel)
+data = Data('EMD Convolutional Autoencoder', 'mixed', noiselevel = noiselevel)
 
 # Specify directory if you have changed folder name / dir
 data.set_ecg_filepath()
@@ -65,12 +65,18 @@ label_dat = data.reformat(label_dat, feature_len = 300, data_form = 2)
 print("Input Data shape: {}".format(np.shape(input_dat)))
 print("Label Data shape: {}".format(np.shape(label_dat)))
 
+# Split data into train and validation set
 train_set, val_set = data.data_splitter(input_dat, label_dat, shuffle = True, ratio = 4)
 
-emd_dir = '{}/EMDs...'.format(os.getcwd())
-
-
-
+# Load IMF outputs of EMD on the trainset
+emd_train = np.load(str(data.filepath) + '/EMDs_{}/EMDs_train.npy'.format(noiselevel))
+emd_val = np.load(str(data.filepath) + '/EMDs_{}/EMDs_val.npy'.format(noiselevel))
+missing_train = np.load(str(data.filepath) + '/EMDs_{}/short_list_train.npy'.format(noiselevel))
+missing_val = np.load(str(data.filepath) + '/EMDs_{}/short_list_val.npy'.format(noiselevel))
+print("Data load complete: EMD file shape | {}".format(np.shape(emd_train)))
+print("Samples with less than 5 EMDs: ", np.shape(missing_train)[0] + np.shape(missing_val)[0])
+print("EMD_ECG size match: ", np.shape(train_set)[0] == np.shape(emd_train)[0]
+      and np.shape(val_set)[0] == np.shape(emd_val)[0])
 print("Step 0: Data Import Done")
 
 EPOCH = 5000
@@ -78,11 +84,13 @@ LR = 0.0003
 BATCH_SIZE = 128
 
 cuda = True if torch.cuda.is_available() else False
-print(cuda)
+print("CUDA: ", cuda)
 
 # Generate tensors for training / validation
 train_set = torch.from_numpy(train_set).float()
 val_set = torch.from_numpy(val_set).float()
+emd_train = torch.from_numpy(emd_train).float()
+emd_val = torch.from_numpy(emd_val).float()
 
 # Autoencoder Model Structure
 class ConvAutoEncoder(nn.Module):
@@ -92,7 +100,7 @@ class ConvAutoEncoder(nn.Module):
         # Zero padding is almost the same as average padding in this case
         # Input = b, 1, 4, 300
         self.encoder = nn.Sequential(
-            nn.Conv2d(1, 8, (4,7), stride=1, padding=(0,1)), # b, 8, 1, 300
+            nn.Conv2d(1, 8, (4,7), stride=1, padding=(0,3)), # b, 8, 1, 300
             nn.Tanh(),
             nn.MaxPool2d((1,2), stride=2), # b, 8, 1, 150
             nn.Conv2d(8, 4, 3, stride=1, padding=1), # b, 8, 1, 150
@@ -119,18 +127,23 @@ CAE1 = ConvAutoEncoder() # IMF 1
 CAE2 = ConvAutoEncoder() # IMF 2
 CAE3 = ConvAutoEncoder() # IMF 3
 loss_func = nn.L1Loss()
-train_loss, val_loss = [], []
-if cuda:
-    CAE1.cuda()
-    CAE2.cuda()
-    CAE3.cuda()
-    loss_func.cuda()
+train_loss, val_loss = [], [] # Train loss is loss of IMF_CAEs. Val loss is reconstruction loss
 
 # Set optimizer
 optimizer1 = torch.optim.Adam(CAE1.parameters(), lr=LR, weight_decay=1e-5)
 optimizer2 = torch.optim.Adam(CAE2.parameters(), lr=LR, weight_decay=1e-5)
 optimizer3 = torch.optim.Adam(CAE3.parameters(), lr=LR, weight_decay=1e-5)
 
+if cuda:
+    CAE1.cuda()
+    CAE2.cuda()
+    CAE3.cuda()
+    optimizer1.cuda()
+    optimizer2.cuda()
+    optimizer3.cuda()
+    loss_func.cuda()
+
+# Define model save function
 def save_model(save_name, optim, loss_f, lr, epoch = EPOCH):
     dir = '{}/Trained_Params/{}/{}_{}'.format(data.filepath, data.model, save_name, epoch)
     if not os.path.exists(dir):
@@ -149,42 +162,36 @@ def save_model(save_name, optim, loss_f, lr, epoch = EPOCH):
     np.save(dir + '/valloss.npy',val_loss)
     print("Step 3: Model Saved")
 
-
 # Train the model
 try:
     # Generates mini_batchs for training. Loads data for validation.
-    train_loader = loader.DataLoader(dataset = train_set, batch_size = BATCH_SIZE, shuffle = True)
+    train_loader = loader.DataLoader(dataset = emd_train, batch_size = BATCH_SIZE, shuffle = True)
 
-    print(np.shape(val_set[:,0:1,:,:]))
-    v_x_IMFs = EMD(val_set[:,0:1,:,:])
-
-    v_x1 = Variable(torch.from_numpy(v_x_IMFs[0]).float())
-    v_x2 = Variable(torch.from_numpy(v_x_IMFs[1]).float())
-    v_x3 = Variable(torch.from_numpy(v_x_IMFs[2]).float())
-
-    v_y = Variable(torch.from_numpy(val_set[:,1:2,:,:]).float())
     # Moves data and model to gpu if available
     if cuda:
-        v_x1, v_x2, v_x3, v_y = v_x1.cuda(), v_x2.cuda(), v_x3.cuda(), v_y.cuda()
+        train_set.cuda()
+        val_set.cuda()
+        emd_train.cuda()
+        emd_val.cuda()
 
     print("Step 2: Model Training Start")
 
     for epoch in range(EPOCH):
         for step, train_data in enumerate(train_loader):
 
-            # Run EMD
-            x_IMFs = EMD(train_data[:,0:1,:,:])
-            y_IMFs = EMD(train_data[:,1:2,:,:])
+            # Load IMFs from pre-run EMD
+            x_IMFs = train_data[:,0:4,:,:] # Adjust to new EMD
+            y_IMFs = train_data[:,4:10,:,:]
 
-            # Construct Tensor Variable for each IMF component
-            b_x1 = Variable(x_IMFs[0]).cuda() if cuda else Variable(x_IMFs[0])
-            b_y1 = Variable(y_IMFs[0]).cuda() if cuda else Variable(y_IMFs[0])
+            # Construct Tensor Variable for each IMF order
+            b_x1 = Variable(x_IMFs[:,0:1,:,:]).cuda() if cuda else Variable(x_IMFs[:,0:1,:,:])
+            b_y1 = Variable(y_IMFs[:,0:1,:,:]).cuda() if cuda else Variable(y_IMFs[:,0:1,:,:])
 
-            b_x2 = Variable(x_IMFs[1]).cuda() if cuda else Variable(x_IMFs[1])
-            b_y2 = Variable(y_IMFs[1]).cuda() if cuda else Variable(y_IMFs[1])
+            b_x2 = Variable(x_IMFs[:,1:2,:,:]).cuda() if cuda else Variable(x_IMFs[:,1:2,:,:])
+            b_y2 = Variable(y_IMFs[:,1:2,:,:]).cuda() if cuda else Variable(y_IMFs[:,1:2,:,:])
 
-            b_x3 = Variable(x_IMFs[2]).cuda() if cuda else Variable(x_IMFs[2])
-            b_y3 = Variable(y_IMFs[2]).cuda() if cuda else Variable(y_IMFs[2])
+            b_x3 = Variable(x_IMFs[:,2:3,:,:]).cuda() if cuda else Variable(x_IMFs[:,2:3,:,:])
+            b_y3 = Variable(y_IMFs[:,2:3,:,:]).cuda() if cuda else Variable(y_IMFs[:,2:3,:,:])
 
             de1, de2, de3 = CAE1(b_x1), CAE2(b_x2), CAE3(b_x3)
 
@@ -203,20 +210,26 @@ try:
             loss3.backward()
             optimizer3.step()
 
-        # Evaluates current model state on val data set
-        pred = CAE1(v_x1).numpy() + CAE2(v_x2).numpy() + CAE3(v_x3).numpy() + v_x_IMFs[3] + v_x_IMFs[4] + v_x_IMFs[5]
-        pred = Variable(torch.from_numpy(pred).float())
-        loss_val_set = loss_func(pred, v_y)
-        print('Epoch: {} | IMF 1 loss: {:.4f} | val loss: {:.4f}'.format(epoch+1, loss1.data[0], loss_val_set.data[0]))
-        train_loss.append((loss1.data[0], loss2.data[1], loss3.data[2]))
-        val_loss.append(loss_val_set.data[0])
+        print('Epoch loss: {} | IMF 1: {:.4f} | IMF 2: {:.4f} | IMF 3: {:.4f}'.format(epoch+1, loss1.data[0], loss2.data[0], loss3.data[0]))
+        train_loss.append((loss1.data[0], loss2.data[0], loss3.data[0]))
+
+        # Evaluates current model state by reconstructing IMFs every 10 epochs
+        if epoch % 10 == 0:
+            train_pred = (CAE1(Variable(emd_train[:,0:1,:,:])).data + CAE2(Variable(emd_train[:,1:2,:,:])).data
+                          + CAE3(Variable(emd_train[:,2:3,:,:])).data + emd_train[:,3:4,:,:])
+            val_pred = (CAE1(Variable(emd_val[:,0:1,:,:])).data + CAE2(Variable(emd_val[:,1:2,:,:])).data
+                        + CAE3(Variable(emd_val[:,2:3,:,:])).data + emd_val[:,3:4,:,:])
+            train_recon_loss = torch.mean(torch.sum(torch.abs(torch.add(-train_pred[:,0,0,:], train_set[:,1,0,:])), dim = 1))
+            val_recon_loss = torch.mean(torch.sum(torch.abs(torch.add(-val_pred[:,0,0,:], val_set[:,1,0,:])), dim = 1))
+            print('Recon loss: {} | Train set: {} | Val set: {}'.format(epoch + 1, train_recon_loss, val_recon_loss))
+            val_loss.append((train_recon_loss,val_recon_loss))
 
     print("Step 2: Model Training Finished")
 
     # Save trained Parameters
 except KeyboardInterrupt:
 
-    if str(input("Save Parameters?(y/n): ")) == 'y':
+    if str(input("Save Parameters?(y/n): ")) != 'n':
         save_name = str(input("Save parameters as?: ")) + '_Interrupted'
         save_model(save_name, 'Adam', 'L1Loss', LR)
     else:
@@ -224,6 +237,6 @@ except KeyboardInterrupt:
 
 else:
     print("entering else statement")
-    save_model('EMD1_nl3', 'Adam', 'L1Loss', LR)
+    save_model('EMD_CDAE_nl{}'.format(noiselevel), 'Adam', 'L1Loss', LR)
     print(os.listdir(os.getcwd()))
     print(os.getcwd())
